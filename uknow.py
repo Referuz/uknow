20df3e341a95de6088f4a13d7cb0e31fd5684412
Referuz <78533108+Referuz@users.noreply.github.com>
2021-10-09 00:41:33 -0500

    Add files via upload

---
+++ uknow.py
@@ -0,0 +1,14 @@
+#Uknow.py
+import pyfiglet
+import random
+
+def otravez() :
+    print("Otra vez [S/N]"
+    return input().lower().startswith("s)
+
+print("Tu numero es:") 
+
+while True:
+    n = random.randint(0, 32)
+    print(n)
+    otravez()
